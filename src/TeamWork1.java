4814165af34b4891439b9fade910d3a4498845f7
kss <kos7114@naver.com>
2023-06-13 16:29:31 +0900

    숫자야구 프로그램 만들기 - 컴퓨터는 0과 9 사이의 서로 다른 숫자 3개를 무작위로 뽑습니다. (ex) 123, 759 - 사용자는 컴퓨터가 뽑은 숫자를 맞추기 위해 시도합니다. - 컴퓨터는 사용자가 입력한 세자리 숫자에 대해서, 아래의 규칙대로 스트라이크(S)와 볼(B)를 알려줍니다.     - 숫자의 값과 위치가 모두 일치하면 S     - 숫자의 값은 일치하지만 위치가 틀렸으면 B - 기회는 무제한이며, 몇번의 시도 후에 맞췄는지 기록됩니다. - 숫자 3개를 모두 맞춘 경우, 게임을 종료합니다.

---
+++ src/TeamWork1.java
@@ -0,0 +1,98 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class TeamWork1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int numlen = 3;
+        String answer = randNums(numlen);
+        System.out.println(answer);
+        System.out.println("컴퓨터가 숫자를 생성하였습니다. 답을 맞춰보세요!");
+        System.out.println("중복없이 " + numlen + "개의 숫자만 입력해주세요.");
+        int ball; //contain
+        int strike; //charAt, indexOf
+        int count = 0;
+        while (true) {
+            String input = br.readLine();
+            try {
+                Integer.parseInt(input);
+                input.charAt(numlen-1);
+                if (input.length() > numlen) {
+                    RuntimeException e3 = new RuntimeException(numlen + "개 이상의 숫자");
+                    throw e3;
+                }
+                if (hasDuplicateCharacters(input)) {
+                    RuntimeException e4 = new RuntimeException("중복인 숫자");
+                    throw e4;
+                }
+            } catch (NumberFormatException e1) {
+                System.out.println("숫자가 아닙니다 : " + e1.getMessage());
+                continue;
+            } catch (StringIndexOutOfBoundsException e2) {
+                System.out.println("숫자가 부족합니다 : " + e2.getMessage());
+                continue;
+            } catch (RuntimeException e3) {
+                System.out.println("중복없이 " + numlen + "개의 숫자만 입력해주세요 : " + e3.getMessage());
+                continue;
+            }
+            ball = 0;
+            strike = 0;
+            StringBuilder sb = new StringBuilder();
+            count++;
+            sb.append(count).append("번째 시도 : ").append(input).append("\n");
+            for (int i = 0; i < 3; i++) {
+                String num = "" + input.charAt(i);
+                if (answer.contains(num)) {
+                    if (answer.indexOf(num) == i) {
+                        strike++;
+                    } else {
+                        ball++;
+                    }
+                }
+            }
+            if (ball == 3) {
+                sb.append(ball).append("B");
+            } else if (strike == 3) {
+                sb.append(strike).append("S").append("\n");
+                sb.append(count).append("번만에 맞히셨습니다.\n게임을 종료합니다.");
+                System.out.println(sb);
+                return;
+            } else {
+                sb.append(ball).append("B").append(strike).append("S");
+            }
+            System.out.println(sb);
+        }
+    }
+
+    private static String randNums(int numlen) {
+        StringBuilder sb = new StringBuilder();
+        int randNum;
+        for (int i = 0; i < numlen; i++) {
+            while (true) {
+                randNum = (int) (Math.random() * 9);
+                if (!sb.toString().contains("" + randNum)) {
+                    break;
+                }
+            }
+            sb.append(randNum);
+        }
+        return sb.toString();
+    }
+
+    public static boolean hasDuplicateCharacters(String str) {
+        StringBuilder sb = new StringBuilder();
+
+        for (char c : str.toCharArray()) {
+            if (sb.toString().contains(""+c)) {
+                // Duplicate character found
+                return true;
+            }
+            sb.append(c);
+        }
+        // No duplicate characters found
+        return false;
+    }
+}
